2b01cd2b80f0dfa103e5f3bf6f8e3c5aea8dba51
Arif Ali <arif97.official@gmail.com>
2026-04-16 22:52:58 +0530

    feat: add android app packaging for mobile testing

--- apps/customer_app/lib/config/app_config.dart
+++ apps/customer_app/lib/config/app_config.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/foundation.dart';
 
 class AppConfig {
+  static const _apiBaseUrlOverride = String.fromEnvironment('APP_API_BASE_URL');
+
   static String get apiBaseUrl {
+    if (_apiBaseUrlOverride.isNotEmpty) {
+      return _apiBaseUrlOverride;
+    }
+
     if (kIsWeb) {
       return 'http://localhost:4000';
     }
---
+++ apps/driver_app/android/app/src/main/kotlin/com/madhupur/driver/madhupur_driver_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.madhupur.driver.madhupur_driver_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
